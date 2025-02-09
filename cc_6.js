b866809c77a7d0f4752e9d3280a5d10a85f9ffbd
Jairo Roa <Jroa@usf.edu>
2025-02-09 17:08:47 -0500

    Task 1 - Business Profit Calculation

---
+++ cc_6.js
@@ -0,0 +1,8 @@
+// Task 1:  Function Declaration
+function calculateProfit(costPrice, sellingPrice, unitSold) {
+    let profit = (sellingPrice - costPrice) * unitSold;
+    console.log('Total Profit: $${proft}');
+}
+
+calculateProfit(20, 30, 100); // Expected output: "Total Profit: $1000"
+calculateProfit(50, 70, 200); // Expected output: "Total Profit: $4000"
