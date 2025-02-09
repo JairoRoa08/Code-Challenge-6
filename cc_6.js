10f70daf113f1611a96159a50fc1bd4edd9fc55d
Jairo Roa <Jroa@usf.edu>
2025-02-09 17:13:27 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,12 @@ function calculateProfit(costPrice, sellingPrice, unitSold) {
 
 calculateProfit(20, 30, 100); // Expected output: "Total Profit: $1000"
 calculateProfit(50, 70, 200); // Expected output: "Total Profit: $4000"
+
+//Task 2: Function Expression
+const calculateSalesTax = function(amount, taxRate) {
+    let salesTax = amount * taxRate;
+    console.log('Sales Tax: $${salesTax}');
+};
+calculateSalesTax(100, 0.07); // Expected output: "Sales Tax: $7"
+calculateSalesTax(500, 0.1);  // Expected output: "Sales Tax: $50"
+
